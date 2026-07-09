5e8f240f7029045b6c5f901dcd0e641401eb4522
liam woodward <liamwoodward20@outlook.com>
2026-07-09 14:15:50 +0100

    upload to github

---
+++ public/js/bag.js
@@ -0,0 +1,89 @@
+const productCards = document.querySelector('.product-cards')
+const bagBtn = document.querySelector('.bi-bag')
+const bagOffScreen = document.querySelector('.shopping-section')
+const bagContainer = document.querySelector('.cart-content')
+
+
+
+const bag = []
+
+
+function sendProductToBag(name, price, cardImg) {
+
+  for (let i = 0; i < bag.length; i++) {
+    if (bag[i].name === name){
+      bag[i].qty += 1
+      return
+    }
+  }
+
+  
+  
+  const item = {
+    name: name,
+    price: price,
+    image: cardImg,
+    qty: 1
+  }
+  bag.push(item)
+}
+
+
+
+function addProduct(event) {
+
+
+  let product = ''
+
+  for (let i = 0; i < bag.length; i++) {
+    product += `
+      <article class="bag-product-card">
+        <div class="item-card-content">
+          <img class="bag-item-image" src="${bag[i].image}">
+          <div class="product-content-container">
+            <h3>${bag[i].name}</h3>
+            <p>${bag[i].price}</p>
+          </div>
+            <div class="quantity-btn-content">
+              <button aria-label="decrease-btn" class="decrease-btn"><i class="bi bi-dash-square"></i></button>
+              <pan class="card-output">${bag[i].qty}</span>
+              <button aria-label="increase-btn" class="increase-btn"><i class="bi bi-plus-square"></i></button>
+            </div>
+        </div>
+      </article>
+    `
+  }
+
+  bagContainer.innerHTML = product
+
+}
+
+
+function displayBag() {
+  if (bagBtn.classList.toggle('enable')) {
+    bagOffScreen.classList.toggle('enable')
+  } else {
+    bagOffScreen.classList.toggle('enable')
+  }
+}
+
+
+
+// this event listener listens for when a product card is being clicked on by the item button
+  productCards.addEventListener('click', function(event){
+  
+  const cardImg = document.getElementsByClassName('card-image')[0].src
+
+  const name = event.target.dataset.name
+  const price = event.target.dataset.price
+  sendProductToBag(name, price, cardImg)
+  addProduct(event)
+  console.log(event.target.dataset.name)
+})
+
+
+
+// this event listener listens for when the bag button is being clicks
+bagBtn.addEventListener('click', function(){
+  displayBag()
+})
